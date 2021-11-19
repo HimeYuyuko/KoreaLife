a22869d86b084adfdfe43c22cb34451b0ef9b964
HimeYuyuko <69159983+HimeYuyuko@users.noreply.github.com>
2021-11-19 13:02:53 +0900

    Delete D3-Spring directory

--- D3-Spring/src/main/java/kribb/d3/repository/filenameDAO.java
+++
@@ -1,9 +0,0 @@
-package kribb.d3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kribb.d3.entity.FileNameInfo;
-
-public interface filenameDAO  extends JpaRepository<FileNameInfo, String>{
-	
-}
